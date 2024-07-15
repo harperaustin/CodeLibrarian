59e9bda1814e5214003c133156b76cdd65213d57
harperaustin <harper_austin@brown.edu>
2024-07-15 12:56:51 -0500

    Added openAI API

--- librarian.py
+++ librarian.py
@@ -5,10 +5,14 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
-
+import openai
+from openai import OpenAI
 
 nlp = spacy.load("en_core_web_md")
 books = pd.read_csv('classics.csv')
+
+openai.api_key = "sk-proj-eFxvJzr1GxiHXK3ZJTagT3BlbkFJOnZDF2hFc11ME7B9AgDF"
+client = OpenAI(api_key="sk-proj-eFxvJzr1GxiHXK3ZJTagT3BlbkFJOnZDF2hFc11ME7B9AgDF")
 
 
 
@@ -37,11 +41,11 @@ def compare_two_books(book1desc, book2desc):
                     d2 = nlp.vocab[desc2]
                     if d2.has_vector:
                         val = d1.similarity(d2)
-                        if val > 0.5:
+                        if val > 0.6:
                              count += 1
                         total += val
-
-    return (count / ((len(book1) + len(book2))/2))
+    return count
+    #return (count / ((len(book1) + len(book2))/2))
 
 
     
@@ -81,7 +85,7 @@ def find_most_similar_book(book_title):
 
     return "The most similar book from the classics dataset is " + books["bibliography.title"][most_sim_index]+ " by "+ books["bibliography.author.name"][most_sim_index] + " with a similarity score of " + str(most_sim_val) + ". The average similarity score was " + str(average)
 
-#print(find_most_similar_book("Normal People"))
+#print(find_most_similar_book("The Call of the Wild"))
 
 def get_book_url(book_title):
      search_url = "https://www.goodreads.com/search?q=" + book_title.replace(' ', '+')
@@ -141,7 +145,43 @@ def get_also_enjoyed_book(book_url):
      rec_book_title = first_rec_title.get_text(strip=True)
      rec_book_author = first_rec_author.get_text(strip=True)
      print(rec_book_title + " by " + rec_book_author)
-          
-     
 
-get_also_enjoyed_book(get_book_url("Normal People"))
+
+def get_stat_diff(book_title):
+     #use "metrics.difficulty.automated readability index" and "metrics.difficulty.linsear write formula"
+
+
+     #first I need to find the index of the book. I alrady use this exact thing, so I can save redundant code somehow.
+    have_book = False
+    book_read = 0
+    book_lins = 0
+    for index, bk_title in enumerate(books["bibliography.title"]):
+        if book_title.lower() == bk_title.lower():
+            have_book = True 
+            book_read = books["metrics.difficulty.automated readability index"][index]
+            book_lins = books["metrics.difficulty.linsear write formula"][index]
+            break
+        
+    smallest_diff = 100
+    closest_index = 0
+    if not have_book:
+          print("Book not in data base, so a book with similar stats can't be calculated")
+          #DO A RANDOM BOOK INSTEAD!!!!
+    else:
+         for index in range(1006):
+              curr_diff = (abs((books["metrics.difficulty.automated readability index"][index] - book_read)) + abs((books["metrics.difficulty.linsear write formula"][index] - book_lins)))
+              if curr_diff < smallest_diff and curr_diff != 0:
+                   closest_index = index
+                   smallest_diff = curr_diff
+
+         print(books["bibliography.title"][closest_index] +  " by " + books["bibliography.author.name"][closest_index] + " with the smallest difference of " + str(smallest_diff))
+              
+              
+#get_stat_diff("Crime and Punishment")
+#client = OpenAI()
+
+response = client.chat.completions.create(model="gpt-3.5-turbo-0125", messages=[{"role": "user", "content": "Tell me why Pride and Prejudice is a good book in 2 sentences."}] )
+
+print(response.choices[0].message.content)
+
+#get_also_enjoyed_book(get_book_url("The Call of the Wild"))
